f92c49e8e4f64f953f891b78de70f051addeb373
lnmput <lnmput@gmail.com>
2024-05-15 18:33:01 +0900

    删除版权信息

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -66,8 +66,7 @@ export default function Editor() {
               </Md2PosterHeader>
               <Md2PosterContent>{mdString}</Md2PosterContent>
               <Md2PosterFooter className='text-center'>
-                <img src="/logo.png" alt="logo" className='inline-block mr-2 w-5' />
-                Powered by BeePoster.com
+                
               </Md2PosterFooter>
             </Md2Poster>
           </div>
